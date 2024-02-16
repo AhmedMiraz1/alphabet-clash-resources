b332b58046009cfac87746d4302f784072c90721
mmirazz <mehedimiraz900@gmail.com>
2024-02-16 22:58:42 +0600

    function added

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -9,17 +9,47 @@
 //     playgroundSection.classList.remove('hidden')
 // }
 
+function handleKeyboardButtonPress(event){
+  const playerPress = event.key;
+
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText
+  const expectedAlphabet = currentAlphabet.toLowerCase()
+  if(playerPress === expectedAlphabet){
+    // Update score
+    // get the current score
+    const currentScoreElement = document.getElementById('current-score');
+    const currentScoreText = currentAlphabetElement.innerText
+    console.log(currentScoreText)
+    // increase the score by 1
+    //  show the update score 
+
+// start a new round 
+    removeBackgroundColorById(expectedAlphabet)
+    continueGame();
+  }else{
+    console.log('You press wrong key')
+  }
+
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
 function continueGame() {
   // step-1 generate a random alphabet.
   const alphabet = getRandomAAlphabet();
+   
   // set randomly  generated alphabet to the screen (show it )
 
   const currentAlphabetElement = document.getElementById("current-alphabet");
   currentAlphabetElement.innerText = alphabet;
+  setBackgroundColorById(alphabet)
+ 
 }
 
 function play() {
   hideElementById("home-section");
   showElementById("playGround-section");
   continueGame();
+ 
 }
